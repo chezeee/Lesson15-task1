a4defabe48a997edf3c3a1b2f9c617174430eb14
Ilya Yalymov <chezeee@gmail.com>
2023-02-05 22:12:57 +0300

    Solution added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
-// define class here
+class User {
+  constructor(firstName, lastName, prefix, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.prefix = prefix;
+    this.age = age;
+  }
+
+  getFullName() {
+    return `${this.prefix} ${this.firstName} ${this.lastName}`;
+  }
+
+  canVote() {
+    return this.age >= 18;
+  }
+}
 
 // Sample usage - do not modify
 const user1 = new User("Kate", "Smith", "Mrs", 20);

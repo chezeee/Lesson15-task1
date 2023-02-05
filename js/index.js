1dc2670afff23c3718271901a778625d06814b99
Ilya Yalymov <chezeee@gmail.com>
2023-02-05 22:11:47 +0300

    Initial structure added

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// define class here
+
+// Sample usage - do not modify
+const user1 = new User("Kate", "Smith", "Mrs", 20);
+user1.getFullName(); // "Mrs. Kate Smith"
+user1.canVote(); // true
+
+const user2 = new User("Alex", "Green", "Mr", 17);
+user2.getFullName(); // "Mr. Alex Green"
+user2.canVote(); // false
